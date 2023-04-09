e1e202132b90f02db2bc0ad82c83ace2d223dd2e
Your Name <you@example.com>
2023-04-09 00:57:30 -0500

    creando el model distrito

---
+++ src/main/java/web/onpe/model/Distrito.java
@@ -0,0 +1,33 @@
+package web.onpe.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Distrito {
+	
+	@Id
+	private int idDistrito;
+	private int idProvincia;
+	private String Detalle;
+	
+	public int getIdDistrito() {
+		return idDistrito;
+	}
+	public void setIdDistrito(int idDistrito) {
+		this.idDistrito = idDistrito;
+	}
+	public int getIdProvincia() {
+		return idProvincia;
+	}
+	public void setIdProvincia(int idProvincia) {
+		this.idProvincia = idProvincia;
+	}
+	public String getDetalle() {
+		return Detalle;
+	}
+	public void setDetalle(String detalle) {
+		Detalle = detalle;
+	}
+	
+}
